c14198a59736d687723b4ebde56b2692312b91ae
AminNeji <mohamedneji2025@gmail.com>
2024-10-06 20:17:50 +0100

    Updated settings for deployment

--- backend/index.js
+++ backend/index.js
@@ -1,24 +1,28 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const cors = require('cors');
 const connectDB = require('./ConnectDB');
-const mongoose = require('mongoose');
 require('dotenv').config();
 
 const TaskRouter = require('./Routes/TaskRoutes');
-// connect DB 
+
 connectDB();
-
-
 
 app.use(cors());
 app.use(express.json());
 
-
-
 app.use('/api/tasks', TaskRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-app.listen(process.env.port, () => {
-    console.log(`server is running on port ${process.env.port}`);
-})
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
